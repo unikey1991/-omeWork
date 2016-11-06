1883dd28d5e8b644c1939419957c79cf1db8b759
unikey1991 <unikey1991@gmail.com>
2016-11-06 13:57:45 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int a = -6;
+
+        String b = (a % 2 == 0)? "четное" : "нечетное";
+        System.out.println(b);
+    }
+}
